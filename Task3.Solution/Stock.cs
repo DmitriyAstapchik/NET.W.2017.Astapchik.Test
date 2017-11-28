68a83464e85a21939e1626a1d052e5a088e05a31
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-28 09:42:48 +0300

    Add files via upload

--- Task3.Solution/Stock.cs
+++ Task3.Solution/Stock.cs
@@ -17,17 +17,12 @@ namespace Task3.Solution
 
         public event EventHandler<MarketEventArgs> NewMarket;
 
-        void OnNewMarket(MarketEventArgs e)
-        {
-            NewMarket?.Invoke(this, e);
-        }
-
         public void Market()
         {
             Random rnd = new Random();
             stocksInfo.USD = rnd.Next(20, 40);
             stocksInfo.Euro = rnd.Next(30, 50);
-            OnNewMarket(new MarketEventArgs(stocksInfo));
+            NewMarket?.Invoke(this, new MarketEventArgs(stocksInfo));
         }
     }
 
