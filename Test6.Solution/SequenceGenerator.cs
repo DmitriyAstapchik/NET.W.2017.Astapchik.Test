5acb0ac7c3975086f8e31e8bc76145680ec3fa9a
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-28 12:16:52 +0300

    Add files via upload

---
+++ Test6.Solution/SequenceGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6.Solution
+{
+    public static class SequenceGenerator<T>
+    {
+        public static IEnumerable<T> Generate(T x1, T x2, uint length, Func<T,T,T> formula)
+        {
+            yield return x1;
+            yield return x2;
+
+            for (uint i = 2; i < length; i++)
+            {
+                yield return x2 = formula(x1, x1 = x2);
+            }
+        }
+    }
+}
