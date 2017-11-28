2b4fac1cd9c62507cf30ac86459cff91c22cb170
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-28 09:13:10 +0300

    Add files via upload

---
+++ Task2.Solution/RandomFileGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Solution
+{
+    public abstract class RandomFileGenerator
+    {
+        public abstract string WorkingDirectory { get; }
+        public abstract string FileExtension { get; }
+        public abstract void GenerateFiles(int filesCount, int contentLength);
+        protected abstract byte[] GenerateFileContent(int contentLength);
+        protected abstract void WriteBytesToFile(string fileName, byte[] content);
+    }
+}
---
+++ Task2.Tests/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Solution;
+
+namespace Task2.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charsGen = new RandomCharsFileGenerator();
+            charsGen.GenerateFiles(2, 20);
+
+            var bytesGen = new RandomBytesFileGenerator();
+            bytesGen.GenerateFiles(3, 30);
+
+            var txtFiles = Directory.GetFiles(charsGen.WorkingDirectory, "*.txt");
+            foreach (var file in txtFiles)
+            {
+                System.Console.WriteLine("txt file length: "+ new FileInfo(file).Length);
+            }
+
+            var bytesFiles = Directory.GetFiles(bytesGen.WorkingDirectory, "*.bytes");
+            foreach (var file in bytesFiles)
+            {
+                System.Console.WriteLine("bytes file length: " + new FileInfo(file).Length);
+            }
+
+            System.Console.Read();
+        }
+    }
+}
