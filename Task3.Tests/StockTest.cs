439fabfcaa8b0a281b17a5d03f3cc00e01cce875
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-29 19:03:32 +0300

    Add files via upload

---
+++ Task3.Tests/StockTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Task3.Solution;
+
+namespace Task3.Tests
+{
+    [TestFixture]
+    public class StockTest
+    {
+        [Test]
+        public void MarketTest()
+        {
+            
+        }
+    }
+}
