4cdba2478a708810ff34dbde74f1fc9f349ff996
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-29 18:58:54 +0300

    Add files via upload

---
+++ Task2.Tests/RandomFileGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Solution;
+using Moq;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class RandomFileGeneratorTests
+    {
+        static RandomFileGenerator gen = Mock.Of<RandomFileGenerator>(gen => gen.WorkingDirectory == "test directory");
+        Mock<RandomFileGenerator> genMock = Mock.Get(gen);
+
+        [Test]
+        public void GenerateFilesTest()
+        {
+            gen.GenerateFiles(1, 10);
+            genMock.VerifyGet(gen => gen.FileExtension, Times.Exactly(1));
+            genMock.VerifyGet(gen => gen.WorkingDirectory, Times.Between(2, 3, Range.Inclusive));
+            genMock.ResetCalls();
+
+            gen.GenerateFiles(2, 20);
+            genMock.VerifyGet(gen => gen.FileExtension, Times.Exactly(2));
+            genMock.VerifyGet(gen => gen.WorkingDirectory, Times.Between(4, 6, Range.Inclusive));
+            genMock.ResetCalls();
+
+            gen.GenerateFiles(10, 10);
+            genMock.VerifyGet(gen => gen.FileExtension, Times.Exactly(10));
+            genMock.VerifyGet(gen => gen.WorkingDirectory, Times.Between(20, 30, Range.Inclusive));
+            genMock.ResetCalls();
+
+            gen.GenerateFiles(0, 10);
+            genMock.VerifyGet(gen => gen.FileExtension, Times.Never);
+            genMock.VerifyGet(gen => gen.WorkingDirectory, Times.Never);
+            genMock.ResetCalls();
+        }
+    }
+}
