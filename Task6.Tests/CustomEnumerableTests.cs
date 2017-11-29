65effcd9f944e53939cab91c05d3b61f26847bc9
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-29 20:20:38 +0300

    styled

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using Test6.Solution;
-using Moq;
 
 namespace Task6.Tests
 {
@@ -26,7 +26,7 @@ namespace Task6.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            CollectionAssert.AreEqual(expected, SequenceGenerator.GenerateSequence(1, 2, 10, (x1, x2) => 6 * x2 - 8 * x1));
+            CollectionAssert.AreEqual(expected, SequenceGenerator.GenerateSequence(1, 2, 10, (x1, x2) => (6 * x2) - (8 * x1)));
         }
 
         [Test]
@@ -34,7 +34,7 @@ namespace Task6.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            var seq = SequenceGenerator.GenerateSequence<double>(1, 2, 10, (x1, x2) => x2 + x1 / x2);
+            var seq = SequenceGenerator.GenerateSequence<double>(1, 2, 10, (x1, x2) => x2 + (x1 / x2));
 
             for (int i = 0; i < 10; i++)
             {
@@ -45,7 +45,7 @@ namespace Task6.Tests
         [Test]
         public void GenerateSequenceExceptionsTest()
         {
-            Assert.Throws<ArgumentNullException>(() => SequenceGenerator.GenerateSequence(null, "asd", 22, Mock.Of<Func<string,string,string>>()));
+            Assert.Throws<ArgumentNullException>(() => SequenceGenerator.GenerateSequence(null, "asd", 22, Mock.Of<Func<string, string, string>>()));
             Assert.Throws<ArgumentNullException>(() => SequenceGenerator.GenerateSequence("f32f", null, 22, Mock.Of<Func<string, string, string>>()));
             Assert.Throws<ArgumentNullException>(() => SequenceGenerator.GenerateSequence("f32f", "fawffw", 22, null));
             Assert.Throws<ArgumentOutOfRangeException>(() => SequenceGenerator.GenerateSequence("f32f", "fawffw", 1, Mock.Of<Func<string, string, string>>()));
