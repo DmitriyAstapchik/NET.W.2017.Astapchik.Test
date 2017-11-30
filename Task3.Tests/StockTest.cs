12f7db60658c82b25a060de019b371c30c69fb02
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-11-30 15:07:16 +0300

    test added

--- Task3.Tests/StockTest.cs
+++ Task3.Tests/StockTest.cs
@@ -15,6 +15,22 @@ namespace Task3.Tests
         [Test]
         public void MarketTest()
         {
+            var infoMock = new Mock<StockInfo>();
+            var argsMock = new Mock<NewMarketEventArgs>(infoMock.Object);
+            argsMock.SetupGet(args => args.Info).Returns(infoMock.Object);
+            var stockMock = new Mock<Stock>();
+            var bank = new Bank("bank", stockMock.Object);
+            var broker = new Broker("broker", stockMock.Object);
+            stockMock.Raise(st => st.NewMarket += null, argsMock.Object);
+            argsMock.VerifyGet(e => e.Info);
+            infoMock.VerifyGet(inf => inf.Euro);
+            infoMock.VerifyGet(inf => inf.USD);
+            argsMock.ResetCalls();
+            infoMock.ResetCalls();
+            broker.StopTrade();
+            stockMock.Raise(st => st.NewMarket += null, argsMock.Object);
+            argsMock.VerifyGet(e => e.Info);
+            infoMock.VerifyGet(inf => inf.Euro);
         }
     }
 }
